136109c2059c16bb35d9a7070dfc91e7c5926b9f
CognitiveGrowth <Falk@Falks-MBP.hsd1.ca.comcast.net>
2017-05-03 23:51:26 -0700

    fixed error in VPI for Bernoulli problem

--- matlab_code/valueOfPerfectInformationBernoulli.m
+++ matlab_code/valueOfPerfectInformationBernoulli.m
@@ -6,7 +6,7 @@ function VPI=valueOfPerfectInformationBernoulli(alpha,beta)
 E_correct=[alpha,beta]/(alpha+beta);
 [max_val,max_pos]=max(E_correct);
 
-EV_original_preference = @(x) (x>0.5)*E_correct(1)+(x<0.5)*E_correct(2);
+EV_original_preference = @(x) (alpha>beta)*x+(beta>alpha)*(1-x)+(alpha==beta)*0.5;
 
 
 VPI = integral(@(x) betapdf(x,alpha,beta).*(max(x,1-x)-EV_original_preference(x)),0,1);    
