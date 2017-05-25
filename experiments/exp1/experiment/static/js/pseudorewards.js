fc53fca2228c6dfd71c7f495eb3fd9ce5fcf5096
CognitiveGrowth <Falk@Falks-MacBook-Pro.local>
2017-05-24 23:37:37 -0700

    thresholding for messages

--- experiments/exp1/experiment/static/js/pseudorewards.js
+++ experiments/exp1/experiment/static/js/pseudorewards.js
@@ -353,7 +353,7 @@ function registerMove(direction){
             }
         }
         
-        if (_.max(Q_moves)>Q_click){
+        if (_.max(Q_moves)>Q_click+0.05){
             observed_too_much = true;
         }
         
@@ -373,7 +373,7 @@ function registerMove(direction){
             Q_moves.push(predictQValue(intermediate_state,available_actions[a]))
         }        
     }    
-    var observed_too_little= (_.max(Q_clicks)> _.max(Q_moves))
+    var observed_too_little= (_.max(Q_clicks)> _.max(Q_moves) + 0.05)
     
     /*
     //feedback message based on full-observation policy:
